b56ff60eb7f89a7a23b1171e9755abe4c2879cde
Paul Charles Leddy <pcharlesleddy@gmail.com>
2012-08-06 21:02:07 +0200

    Fix if statement

--- monit_check_replication.rb
+++ monit_check_replication.rb
@@ -2,16 +2,10 @@
 
 slaveStatus = %x[HOME=/root mysql -e 'show slave status\\G']
 File.open('/var/tmp/a', 'w') {|f| f.write(slaveStatus) }
-if 
-  slaveStatus.match('Slave_IO_Running: Yes') and 
-  #slaveStatus.match('Slave_IO_Running: No') and 
-  slaveStatus.match('Slave_SQL_Running: Yes') and
-  slaveStatus.match('Seconds_Behind_Master: 0') do 
-  $stderr.puts("RUNNING")
+if slaveStatus.match('Slave_IO_Running: Yes') and slaveStatus.match('Slave_SQL_Running: Yes') and ( slaveStatus.match(/Seconds_Behind_Master: (\d+)/)[1].to_i < 100 ) 
+  $stderr.puts("RUNNING:" + slaveStatus.match(/Seconds_Behind_Master: (\d+)/)[1])
   exit(0)
-end
 else 
-  $stderr.puts("STOPPED")
+  $stderr.puts("STOPPED: " + slaveStatus.match(/Seconds_Behind_Master: (\d+)/)[1])
   exit(1)
 end
-
