5c6bdf590d017d21fbb8461cec1e990d3e1ad35f
pcharlesleddy <pcharlesleddy@gmail.com>
2012-12-10 13:01:10 +0100

    Update email-strip.pl

--- email-strip.pl
+++ email-strip.pl
@@ -10,12 +10,11 @@ my $email_dir = '/home/paul/aa/tmpF';
 my $f = File::Util->new();
 my @files = $f->list_dir($email_dir,'--files-only');
 my $data = {};
-@files = @files[ 0 .. 2 ];
+@files = @files[0..5];
 
 foreach my $file (@files) {
 
     next if ($file !~ /\.eml$/);
-
 
     my $f = File::Util->new();
     my $contents = $f->load_file($file);
@@ -24,19 +23,19 @@ foreach my $file (@files) {
     $data->{$digest}->{'filename'} = $file;
 
     my $email = Email::Simple->new($contents);
-    $data->{$digest}->{'from'} = $email->header("From");
-    $data->{$digest}->{'to'} = $email->header("To");
-    $data->{$digest}->{'cc'} = $email->header("CC");
-    $data->{$digest}->{'x'} = $email->header("X-Uniform-Type-Identifier");
+    $data->{$digest}->{'header'}->{'from'} = $email->header("From");
+    $data->{$digest}->{'header'}->{'to'} = $email->header("To");
+    $data->{$digest}->{'header'}->{'cc'} = $email->header("CC");
+    $data->{$digest}->{'header'}->{'x'} = $email->header("X-Uniform-Type-Identifier");
 
-    $data->{$digest}->{'to'} =  defined($data->{$digest}->{'to'}) ? [split( /,\s*/, $data->{$digest}->{'to'})] : [];
-    $data->{$digest}->{'cc'} =  defined($data->{$digest}->{'cc'}) ? [split( /,\s*/, $data->{$digest}->{'cc'})] : [];
+    $data->{$digest}->{'to'} =  defined($data->{$digest}->{'header'}->{'to'}) ? [split( /,\s*/, $data->{$digest}->{'header'}->{'to'})] : [];
+    $data->{$digest}->{'cc'} =  defined($data->{$digest}->{'header'}->{'cc'}) ? [split( /,\s*/, $data->{$digest}->{'header'}->{'cc'})] : [];
 
     $pattern = '\@glossybox\.ca';
-    $data->{$digest}->{'match'}->{'from'} = ($data->{$digest}->{'from'} =~ /$pattern/) ? 1 : 0;
+    $data->{$digest}->{'match'}->{'from'} = ($data->{$digest}->{'header'}->{'from'} =~ /$pattern/) ? 1 : 0;
     $data->{$digest}->{'match'}->{'to'} = scalar(grep( /$pattern/, @{$data->{$digest}->{'to'}}));
     $data->{$digest}->{'match'}->{'cc'} = scalar(grep( /$pattern/, @{$data->{$digest}->{'cc'}}));
-    $data->{$digest}->{'match'}->{'x'} = defined($data->{$digest}->{'x'}) ? 1 : 0;
+    $data->{$digest}->{'match'}->{'x'} = defined($data->{$digest}->{'header'}->{'x'}) ? 1 : 0;
 
     $data->{$digest}->{'match'}->{'have_internal_copy'} = (
         $data->{$digest}->{'match'}->{'x'}
@@ -46,7 +45,7 @@ foreach my $file (@files) {
         or ( $data->{$digest}->{'match'}->{'cc'} > 1 )
     )
         ? 1
-        : 0
+        : 0;
 }
 
 print "\n";
