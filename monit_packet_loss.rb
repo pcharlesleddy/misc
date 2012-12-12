7364aeb252b9f49d394927998d03fe639a52f15f
pcharlesleddy <pcharlesleddy@gmail.com>
2012-12-12 12:58:03 +0100

    Update monit_packet_loss.rb

--- monit_packet_loss.rb
+++ monit_packet_loss.rb
@@ -1,9 +1,13 @@
-packet_loss = %x[ping -q -c 10 vpn.corp.glossybox.net | grep 'packet loss' | awk -F, '{print $3}' | awk -F% '{print $1}' | tr -d ' ']
+#!/usr/bin/ruby
 
-if packet_loss.to_i > 2 
-  $stderr.puts('Packet loss high: ' + packet_loss.chomp + '%')
+host = ARGV[0]
+
+packet_loss = %x[ping -q -c 10 #{host} | grep 'packet loss' | awk -F, '{print $3}' | awk -F% '{print $1}' | tr -d ' ']
+
+if packet_loss.to_i > 0
+  $stderr.puts('Packet loss high: ' + packet_loss.chomp + '%' + " (#{host})")
   exit(1)
 else
-  $stderr.puts('Packet loss OK: ' + packet_loss.chomp + '%')
+  $stderr.puts('Packet loss OK: ' + packet_loss.chomp + '%' + " (#{host})")
   exit(0)
 end
