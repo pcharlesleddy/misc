3e250acce5b934387e347407b0140c34e634508b
pcharlesleddy <pcharlesleddy@gmail.com>
2012-12-12 02:49:34 -0800

    Create monit_packet_loss.rb

---
+++ monit_packet_loss.rb
@@ -0,0 +1,9 @@
+packet_loss = %x[ping -q -c 10 vpn.corp.glossybox.net | grep 'packet loss' | awk -F, '{print $3}' | awk -F% '{print $1}' | tr -d ' ']
+
+if packet_loss.to_i > 2 
+  $stderr.puts('Packet loss high: ' + packet_loss)
+  exit(1)
+else
+  $stderr.puts('Packet loss OK: ' + packet_loss)
+  exit(0)
+end
