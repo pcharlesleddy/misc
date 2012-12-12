54ba02640ce3803d5d92a147f7637c71978b23a4
pcharlesleddy <pcharlesleddy@gmail.com>
2012-12-12 12:06:06 +0100

    Update monit_latency.rb

--- monit_latency.rb
+++ monit_latency.rb
@@ -3,9 +3,9 @@
 latency = %x[ping -q -c 3 vpn.corp.glossybox.net | grep 'rtt' | awk -F= '{print $2}' | awk -F/ '{print $2}' | tr -d ' ']
 
 if latency.to_i > 15 
-  $stderr.puts('Latency high: ' + latency)
+  $stderr.puts('Latency high: ' + latency.chomp + ' seconds')
   exit(1)
 else
-  $stderr.puts('Latency OK: ' + latency)
+  $stderr.puts('Latency OK: ' + latency.chomp + ' seconds')
   exit(0)
 end
