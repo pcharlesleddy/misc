27afe1bd234770cde20860698c3ecb5b5faf453b
pcharlesleddy <pcharlesleddy@gmail.com>
2012-12-12 12:06:58 +0100

    Update monit_packet_loss.rb

--- monit_packet_loss.rb
+++ monit_packet_loss.rb
@@ -1,9 +1,9 @@
 packet_loss = %x[ping -q -c 10 vpn.corp.glossybox.net | grep 'packet loss' | awk -F, '{print $3}' | awk -F% '{print $1}' | tr -d ' ']
 
 if packet_loss.to_i > 2 
-  $stderr.puts('Packet loss high: ' + packet_loss)
+  $stderr.puts('Packet loss high: ' + packet_loss.chomp + '%')
   exit(1)
 else
-  $stderr.puts('Packet loss OK: ' + packet_loss)
+  $stderr.puts('Packet loss OK: ' + packet_loss.chomp + '%')
   exit(0)
 end
