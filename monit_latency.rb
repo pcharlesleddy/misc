afadc83de3706498d3d1aab2c6c5afb296d96fc8
pcharlesleddy <pcharlesleddy@gmail.com>
2012-12-12 13:05:33 +0100

    doh

--- monit_latency.rb
+++ monit_latency.rb
@@ -5,9 +5,9 @@ host = ARGV[0]
 latency = %x[ping -q -c 3 #{host} | grep 'rtt' | awk -F= '{print $2}' | awk -F/ '{print $2}' | tr -d ' ']
 
 if latency.to_i > 30 
-  $stderr.puts('Latency high: ' + latency.chomp + ' seconds' + " (#{host})" )
+  $stderr.puts('Latency high: ' + latency.chomp + ' ms' + " (#{host})" )
   exit(1)
 else
-  $stderr.puts('Latency OK: ' + latency.chomp + ' seconds' + " (#{host})")
+  $stderr.puts('Latency OK: ' + latency.chomp + ' ms' + " (#{host})")
   exit(0)
 end
